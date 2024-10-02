30b26dfb1175b7dccef4f5998ae00dab8c37d926
adrianql5 <adrianquiroga5@gmail.com>
2024-10-02 15:31:08 +0200

    buclw while

---
+++ cliente.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <inttypes.h>
+
+
+
+
+int main(int argc, char const *argv[]){
+    return 0;
+}
